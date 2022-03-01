4fb2ea0f97b1fb155d3a0e7747e9042e6cd25db5
Alexey <stalpetc@gmail.com>
2022-03-01 17:27:42 +0300

    feat: add deletModal components

--- src/components/MovieCard/movieCard.js
+++ src/components/MovieCard/movieCard.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MovieCard.css";
+import DeleteModal from "../deletModal/deletModal";
 
 const MovieCard = (props) => {
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+
+  const handleDeleteModal = () => {
+    setOpenDeleteModal(!openDeleteModal);
+  }
 
   return (
     <div className="movie-wrap">
       <div className="movie-poster">
         <img className="movie-card" id={props.data.id} src={props.data.poster_path} alt={props.data.title} />
+
+        {!props.showOptions ?
+          <div className="dot" onClick={(event) => props.handleClick(event, props.data.id)}></div>
+          : <div className="conteiner-modal">
+            <div className="modal-close">x</div>
+            <div className="modal-edit">Edit</div>
+            <div className="modal-delete" onClick={handleDeleteModal}>Delete</div>
+          </div>}
+        {openDeleteModal && <DeleteModal handleDeleteModal={handleDeleteModal} />}
+
       </div>
       <div className="movie-description">
         <div className="movie-title">
           <h3>{props.data.title}</h3>
-          <p className="movie-date-realise">{props.data.release_date}</p>
+          <p className="movie-date-realise">{props.data.release_date.split('-')[0]}</p>
         </div>
-        <div className="movie-genre">{props.data.genres}</div>
+        <div className="movie-genre">{props.data.genres.join(', ')}</div>
       </div>
     </div>
   );
---
+++ src/components/deletModal/deletModal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./deletModal.css";
+
+const DeleteModal = (props) => {
+    const modalRef = React.createRef();
+  
+    const handleOutsideClick = (event) => {
+      if (modalRef.current.contains(event.target)) {
+        return;
+      }
+      props.handleDeleteModal();
+    }
+  
+    return (
+      <div className="modal">
+        <div className="modal-wrapper" onClick={handleOutsideClick}>
+          <div className="modal-container" ref={modalRef}>
+            <div className="modal-close" onClick={props.handleDeleteModal}></div>
+            <div className="modal-content">
+              <h2 className="modal-title">Delete movie</h2>
+              <p className="modal-subtitle">Are you sure you want to delete this movie?</p>
+            </div>
+            <div className="modal-button">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default DeleteModal;
