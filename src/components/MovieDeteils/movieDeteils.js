c596c42c81a574dae93a5ec8b57e4581761376be
Alexey <stalpetc@gmail.com>
2022-03-18 14:42:43 +0300

    feat: add marcap in new component

---
+++ src/components/MovieDeteils/movieDeteils.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const MovieDetails = () => {
+  
+  return (
+    <div className="movieDetails">
+      <div className="details">
+        <div className="details-header">
+          <button className="details-search" type="button"></button>
+        </div>
+        <div className="details-container">
+          <div className="details-image">
+            <img src='#' className="image" />
+          </div>
+
+          <div className="details-content">
+
+            <div className="content-title">
+              <h2 className="content-movieTitle">title</h2>
+              <div className="content-voteAverage">average</div>
+            </div>
+
+            <div className="content-tagline">tagline</div>
+
+            <div className="content-info">
+              <span className="content-infoDate">releaseDate</span>
+              <span className="content-infoRuntime">runtime min</span>
+            </div>
+
+            <div className="content-overview">overview</div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieDetails;
