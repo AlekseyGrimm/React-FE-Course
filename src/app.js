404f82f68eb2469341b932be14019ea9badef1de
Alexey <stalpetc@gmail.com>
2022-03-21 15:01:50 +0300

    fix: fix bags

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ErrorBoundary from "./components/errorBoundary/errorBoundary";
 import Header from "./components/header/Header";
 import Body from "./components/Body/body";
 import TopText from "./components/topText/topText";
-import MovieDetails from "./components/MovieDeteils/movieDeteils";
+import MovieDetails from "./components/MovieDeteils/MovieDeteils";
 
 const App = () => {
   return (
